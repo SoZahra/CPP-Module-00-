a4e4962ed567a0b3b69ef812b74ba1264fcd8225
Fatima Zahra Zayani <fzayani@bess-f4r2s7.clusters.42paris.fr>
2025-02-04 19:04:10 +0100

    ex01

---
+++ ex01/contact.hpp
@@ -0,0 +1,94 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   contact.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fzayani <fzayani@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/04 17:52:03 by fzayani           #+#    #+#             */
+/*   Updated: 2025/02/04 18:51:34 by fzayani          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+# ifndef MAX_CONTACT
+ # define MAX_CONTACT 8
+# endif
+
+class Contact
+{
+	public:
+		void set_num(std::string num);
+		std::string get_num() const;
+
+		void set_secret(std::string secret);
+		std::string get_secret() const;
+
+		void set_firstN(std::string firstN);
+		std::string get_firstN() const;
+
+		void set_lastN(std::string lastN);
+		std::string get_lastN() const;
+
+		void set_nikename(std::string nikename);
+		std::string get_nikename() const;
+
+	private:
+
+		std::string num;
+		std::string secret;
+		std::string firstN;
+		std::string lastN;
+		std::string nikename;
+};
+
+void Contact::set_num(std::string num)
+{
+	this->num = num;
+}
+
+std::string Contact::get_num() const
+{
+	return this->num;
+}
+
+void Contact::set_secret(std::string secret)
+{
+	this->secret = secret;
+}
+
+std::string Contact::get_secret() const
+{
+	return this->secret;
+}
+
+void Contact::set_firstN(std::string firstN)
+{
+	this->firstN = firstN;
+}
+
+std::string Contact::get_firstN() const
+{
+	return this->firstN;
+}
+
+void Contact::set_lastN(std::string lastN)
+{
+	this->lastN = lastN;
+}
+
+std::string Contact::get_lastN() const
+{
+	return this->lastN;
+}
+
+void Contact::set_nikename(std::string nikename)
+{
+	this->nikename = nikename;
+}
+
+std::string Contact::get_nikename() const
+{
+	return this->nikename;
+}
---
+++ ex01/phonebook.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   phonebook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fzayani <fzayani@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/04 17:52:29 by fzayani           #+#    #+#             */
+/*   Updated: 2025/02/04 18:31:57 by fzayani          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+# ifndef MAX_CONTACT
+ # define MAX_CONTACT 8
+# endif
+
+class PhoneBook
+{
+	int tab_contact[8];
+
+public :
+	void ADD();
+	void SEARCH();
+};
+
+void PhoneBook::ADD()
+{
+
+}
+
+void PhoneBook::SEARCH()
+{
+
+}
